006fe3603c49a4a373f5975d20554431d3c09233
ProgKenhy <sashok20053@gmail.com>
2024-11-02 00:38:25 +0200

    time checker and quick sort

---
+++ 4_sortingMethods/11_fast.py
@@ -0,0 +1,47 @@
+import random
+import timeit
+
+
+def partition(arr, low, high):
+    pivot = arr[(low + high) // 2]
+    i = low - 1
+    j = high + 1
+    while True:
+        i += 1
+        while arr[i] < pivot:
+            i += 1
+
+        j -= 1
+        while arr[j] > pivot:
+            j -= 1
+
+        if j <= i:
+            return j
+
+        arr[i], arr[j] = arr[j], arr[i]
+
+
+def quick_sort(arr):
+    def _quick_sort(arr, low, high):
+        if low < high:
+            split_index = partition(arr, low, high)
+            _quick_sort(arr, low, split_index)
+            _quick_sort(arr, split_index + 1, high)
+
+    _quick_sort(arr, 0, len(arr) - 1)
+
+
+array = [random.randint(0, 100000000) for _ in range(1000000)]
+
+
+def test_large_quick_sort():
+    large_array = array.copy()
+    quick_sort(large_array)
+
+
+execution_time = timeit.timeit(test_large_quick_sort, number=1)
+print(f"Среднее время выполнения на массиве из 1,000,000 элементов: {execution_time / 1:.6f} секунд")
+
+array = [10, 1, 3, 6, -5, 99, 0, 13, 12]
+quick_sort(array)
+print(array)
--- 4_sortingMethods/6_selection.py
+++ 4_sortingMethods/6_selection.py
@@ -1,3 +1,7 @@
+import random
+import timeit
+
+
 def selection_sort(arr):
     for i in range(len(arr)):
         min_index = i
@@ -9,5 +13,16 @@ def selection_sort(arr):
     return arr
 
 
-array = [10, 3, 5, 7, 1, 2, 76, 1, 5, 7, 1, 3, 6, 8, 89, 45, 3, 2, 56, 87, 9, 9, 7, 123, 5, 34, 32, 3, 6, 8]
-print(selection_sort(array))
+def test_large_quick_sort():
+    large_array = array.copy()
+    selection_sort(large_array)
+
+
+array = [random.randint(0, 100000000) for _ in range(1000)]
+
+execution_time = timeit.timeit(test_large_quick_sort, number=1)
+print(f"Среднее время выполнения на массиве из 1,000,000 элементов: {execution_time / 1:.6f} секунд")
+
+array = [10, 1, 3, 6, -5, 99, 0, 13, 12]
+selection_sort(array)
+print(array)
